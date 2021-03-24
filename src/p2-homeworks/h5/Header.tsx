d8abbd76e08f3b94fa3c206d14e7dcd8ae3ce555
Lykke27 <usachev97@gmail.com>
2021-03-24 21:30:53 +0300

    add new pages and navigation

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <div>
+                <NavLink to="/pre-junior" >Pre Junior</NavLink>
+            </div>
+            <div>
+                <NavLink to="/junior" >Junior</NavLink>
+            </div>
+            <div>
+                <NavLink to="/junior-plus" >Junior Plus</NavLink>
+            </div>
 
         </div>
     );
